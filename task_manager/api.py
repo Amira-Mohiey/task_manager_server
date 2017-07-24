c6013032f9f1716954a49f5e244a9a91cd5b66a9
Amira-Mohiey <amira.mohiey@cartologic.com>
2017-07-24 13:41:16 +0200

    adding filters

--- task_manager/api.py
+++ task_manager/api.py
@@ -1,3 +1,4 @@
+from tastypie.constants import ALL_WITH_RELATIONS,ALL
 from tastypie.resources import ModelResource
 from tastypie.authorization import  Authorization
 from tastypie.authentication import BasicAuthentication
@@ -7,17 +8,27 @@ from tastypie import fields
 from .customAuth import CustomAuthorization
 
 class UserResource(ModelResource):
+    # tasks = fields.ToManyField('task_manager.api.resources.TaskResource','tasks', null=True, blank=True)
     class Meta:
         queryset = User.objects.all()
-        authorization =  Authorization()
+        authorization = Authorization()
+        filtering = {
+            'username': ALL
+        }
         authentication = BasicAuthentication()
         resource_name = 'user'
 
+
 class TaskResource(ModelResource):
     user = fields.ForeignKey(UserResource, 'created_by')
+
     class Meta:
         queryset = Task.objects.all()
         resource_name = 'task'
+        filtering = {
+            'user': ALL_WITH_RELATIONS
+        }
+        fields = ['user', 'task_title', 'task_description']
         authorization = CustomAuthorization()
-        authentication=BasicAuthentication()
-        allowed_methods = ['get','post','put','delete']
+        authentication = BasicAuthentication()
+        allowed_methods = ['get', 'post', 'put', 'delete']
--- task_manager/customAuth.py
+++ task_manager/customAuth.py
@@ -6,19 +6,20 @@ class CustomAuthorization(Authorization):
     def read_list(self, object_list, bundle):
         # This assumes a ``QuerySet`` from ``ModelResource``.
         print(bundle.request.user)
-        return object_list.filter(created_by=bundle.request.user)
+        return object_list
+        # return object_list.filter(created_by=bundle.request.user)
 
     def read_detail(self, object_list, bundle):
         # Is the requested object owned by the user?
-        return bundle.obj.created_by == bundle.request.user
-
+        # return bundle.obj.created_by == bundle.request.user
+        pass
     def create_list(self, object_list, bundle):
         # Assuming they're auto-assigned to ``user``.
         return object_list
 
     def create_detail(self, object_list, bundle):
-        return bundle.obj.created_by == bundle.request.user
-
+        # return bundle.obj.created_by == bundle.request.user
+        pass
     def update_list(self, object_list, bundle):
         allowed = []
 
@@ -30,11 +31,12 @@ class CustomAuthorization(Authorization):
         return allowed
 
     def update_detail(self, object_list, bundle):
-        return bundle.obj.created_by == bundle.request.user
-
+        # return bundle.obj.created_by == bundle.request.user
+        pass
     def delete_list(self, object_list, bundle):
 
         return bundle.obj.created_by == bundle.request.user
 
     def delete_detail(self, object_list, bundle):
-        return bundle.obj.created_by == bundle.request.user
+        # return bundle.obj.created_by == bundle.request.user
+        pass
